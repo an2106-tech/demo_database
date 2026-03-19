f0a88b49bb43375a8c0db97fd2bd413a443ef50b
anhtu63-dev <themoba777@gmail.com>
2026-03-19 11:03:14 +0700

    [DB] - Thêm Departments module (Model - Migrate - Seeder - Factory

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Department;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Department::factory()->count(10)->create();
+    }
+}
