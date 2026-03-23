4d32fd56126100eb1055f434575df9a5c05442d3
anhtu63-dev <themoba777@gmail.com>
2026-03-23 21:34:43 +0700

    Tạo bảng applications flow: scorecard_templates

---
+++ database/migrations/2026_03_23_143247_create_scorecard_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scorecard_templates', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('name'); 
+            $table->json('criteria'); 
+            $table->boolean('is_default')->default(0); 
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete(); 
+            $table->timestamps(); 
+            $table->softDeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scorecard_templates');
+    }
+};
