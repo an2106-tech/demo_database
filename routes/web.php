84dad53131b2a1d7d8b8b7f26380d7819eee89e8
QuangDong-dev <donglequang26@gmail.com>
2026-03-27 22:45:16 +0700

    hoàn thiện giao diện message và cập nhật company

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Livewire\Client\BrowseCompanies;
 use App\Livewire\Client\BrowseJobs;
 use App\Livewire\Client\CandidatesDetails;
 use App\Livewire\Client\employers\BrowseCandidates;
+use App\Livewire\Client\Employers\CompanyProfile;
 use App\Livewire\Client\Employers\EmployersDashboard;
+use App\Livewire\Client\Employers\Message;
 use App\Livewire\Client\Employers\PostJob;
 use App\Livewire\Client\Employers\SingleCompany;
 use App\Livewire\Client\Home;
@@ -31,7 +33,8 @@ Route::prefix('employers')->name('employers.')->group(function () {
     Route::get('/single_company', SingleCompany::class)->name('single_company');
     Route::get('/post_job', PostJob::class)->name('post_job');
     Route::get('/dashboard', EmployersDashboard::class)->name('dashboard');
-    Route::get('/company-profile', EmployersDashboard::class)->name('company_profile');
+    Route::get('/company-profile', CompanyProfile::class)->name('company_profile');
+    Route::get('/message', Message::class)->name('message');
 });
 
 
