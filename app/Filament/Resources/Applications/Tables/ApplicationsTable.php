7b88e959c404387e4ab45f1e5b714b3524368199
anhtu63-dev <themoba777@gmail.com>
2026-03-27 23:10:22 +0700

    Filament Shield: Super Admin

---
+++ app/Filament/Resources/Applications/Tables/ApplicationsTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Applications\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+
+use Filament\Tables\Table;
+
+class ApplicationsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
