398baf32bab85a248d1131792dfda830c6bd4da4
Kimanh278 <kimanhdang278@gmail.com>
2026-03-29 12:27:35 +0700

    Cắt giao diện Page

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ use App\Livewire\Client\pages\Login;
 use App\Livewire\Client\pages\Register as PagesRegister;
 use App\Livewire\Client\pages\Single;
 use App\Livewire\Client\Sidebars;
+use App\Livewire\Client\SignUp;
+use App\Livewire\Client\SubmitResume;
 use Illuminate\Container\Attributes\Log;
 use Illuminate\Support\Facades\Route;
 Route::get('/', Home::class)->name('home');
