28f2b0d6c6c8a8cdb542c40522645bf88669d2c3
an2106-tech <trongan456nc@gmail.com>
2026-03-30 10:27:32 +0700

    cập nhâ hiển thị branche trong deparrtment

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Branch;
 use App\Models\Department;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,27 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        Department::factory()->count(10)->create();
+        $branchIds = Branch::query()->pluck('id');
+
+        if ($branchIds->isEmpty()) {
+            $this->call(BranchSeeder::class);
+            $branchIds = Branch::query()->pluck('id');
+        }
+
+        Department::query()
+            ->whereNull('branch_id')
+            ->get()
+            ->each(function (Department $department) use ($branchIds): void {
+                $department->update([
+                    'branch_id' => $branchIds->random(),
+                ]);
+            });
+
+        Department::factory()
+            ->count(10)
+            ->state(fn () => [
+                'branch_id' => $branchIds->random(),
+            ])
+            ->create();
     }
 }
