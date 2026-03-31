f3d55b11e2cf72a73f6f8b8577c1d39f69a6058f
QuangDong-dev <donglequang26@gmail.com>
2026-03-31 23:02:11 +0700

    [CLIENT] Đổ dữ liệu trang quản lý ứng viên

--- app/Livewire/Client/employers/ManageCandidate.php
+++ app/Livewire/Client/employers/ManageCandidate.php
@@ -4,12 +4,14 @@ namespace App\Livewire\Client\Employers;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use App\Models\Candidate;
 
 class ManageCandidate extends Component
 {
     #[Layout('layouts.client')]
     public function render()
     {
-        return view('livewire.client.employers.manage_candidate');
+        $candidates = Candidate::latest()->get();
+        return view('livewire.client.employers.manage_candidate', ['candidates' => $candidates]);
     }
 }
